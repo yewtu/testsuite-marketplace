cae86850c5651903b6ec06175559c540f67e55fa
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-07-25 23:03:10 +0100

    Nightwatch Chromedriver CI config

--- browser-tests/helpers/external-globals.js
+++ browser-tests/helpers/external-globals.js
@@ -1,13 +1,43 @@
 var chromedriver = require('chromedriver');
+var path = require('path');
+var driverInstanceCI;
+
+function isRunningInCI() {
+	return this.test_settings.globals.integration;
+}
+
+function startChromeDriver() {
+	if (isRunningInCI.call(this)) {
+		var location = path.join(__dirname, '../bin/chromedriver-linux64-2.17');
+		driverInstanceCI = require('child_process').execFile(location, []);
+		return;
+	}
+
+	chromedriver.start();
+}
+
+function stopChromeDriver() {
+	if (isRunningInCI.call(this)) {
+		driverInstanceCI && driverInstanceCI.kill();
+		return;
+	}
+
+	chromedriver.stop();
+}
+
 module.exports = {
+	'ci-server' : {
+		integration : true
+	},
+
 	before : function(done) {
-		chromedriver.start();
+		startChromeDriver.call(this);
 
 		done();
 	},
 
 	after : function(done) {
-		chromedriver.stop();
+		stopChromeDriver.call(this);
 
 		done();
 	}
