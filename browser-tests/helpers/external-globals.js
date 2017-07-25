f016e1216c989f61ff024303916c0f812f693c9e
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2017-07-25 23:10:58 +0100

    Revert "Nightwatch Chromedriver CI config"
    This reverts commit cae86850c5651903b6ec06175559c540f67e55fa.

--- browser-tests/helpers/external-globals.js
+++ browser-tests/helpers/external-globals.js
@@ -1,43 +1,13 @@
 var chromedriver = require('chromedriver');
-var path = require('path');
-var driverInstanceCI;
-
-function isRunningInCI() {
-	return this.test_settings.globals.integration;
-}
-
-function startChromeDriver() {
-	if (isRunningInCI.call(this)) {
-		var location = path.join(__dirname, '../bin/chromedriver-linux64-2.17');
-		driverInstanceCI = require('child_process').execFile(location, []);
-		return;
-	}
-
-	chromedriver.start();
-}
-
-function stopChromeDriver() {
-	if (isRunningInCI.call(this)) {
-		driverInstanceCI && driverInstanceCI.kill();
-		return;
-	}
-
-	chromedriver.stop();
-}
-
 module.exports = {
-	'ci-server' : {
-		integration : true
-	},
-
 	before : function(done) {
-		startChromeDriver.call(this);
+		chromedriver.start();
 
 		done();
 	},
 
 	after : function(done) {
-		stopChromeDriver.call(this);
+		chromedriver.stop();
 
 		done();
 	}
